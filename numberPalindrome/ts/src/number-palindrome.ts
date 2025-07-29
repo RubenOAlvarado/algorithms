d1f1a9f9f1f186f5cb5eb01d069b3ef17110b8ca
Ruben Alvarado <rubenosmaralvarado@gmail.com>
2025-07-29 10:04:23 -0600

    fix: correct variable usage in isPalindrome function for clarity and accuracy

--- numberPalindrome/ts/src/number-palindrome.ts
+++ numberPalindrome/ts/src/number-palindrome.ts
@@ -1,16 +1,17 @@
 export function isPalindrome(num: number): boolean{
     // 1. Initialize a variable `original` to the input number.
-    let original = Math.abs(num);
+    let temp = Math.abs(num);
     // 2. Initialize a variable `reversed` to 0.
     let reversed = 0;
+    const original = temp;
     // 3. While `original` is greater than 0:
-    while( original > 0){
+    while( temp > 0){
         // - Get the last digit of the number (using `num % 10`).
-        const digit = original % 10;
+        const digit = temp % 10;
         // - Add this digit to `reversed` (multiply `reversed` by 10 and add the last digit).
         reversed = reversed * 10 + digit;
         // - Remove the last digit from the number (using `num //= 10`).
-        original = Math.floor(original / 10);
+        temp = Math.floor(temp / 10);
     }
 
     // 4. After the loop, compare `original` with `reversed`.
